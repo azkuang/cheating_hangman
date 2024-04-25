6053f0f75826b42380507cb2b2af74ed4fbc6064
unknown <alexzkuang0314@gmail.com>
2024-04-25 16:19:08 -0400

    Starting files

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Creating a game of cheating hangman in C++. The goal of the game is to not let t
 
 Find a large word file that has many options of words to choose from. Plan out steps and different methods to implement to create the cheating hangman.
 
+Downloaded a txt file of words from: https://github.com/Xethron/Hangman/blob/master/words.txt
+
 **Solution**
 
 
--- main.cpp
+++ main.cpp
@@ -2,9 +2,30 @@
 // Authors: Alex Kuang, Mason Cormany
 
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <list>
+#include <string.h>
+#include <string>
 
 using namespace std;
 
 int main() {
+    list <string> textList;
+    string text;
+    ifstream MyReadFile("words.txt");
+
+    // Reading in file
+    while(getline (MyReadFile, text)) {
+        textList.push_front(text);
+    }
+    MyReadFile.close();
+
+    for (auto w:textList) {
+        cout << w << endl;
+    }
+
     return 0;
 }
