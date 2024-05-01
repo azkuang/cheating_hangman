eaa32201a48173772cca12a7f0443352b3a22d58
kaotickoopa <mcormany@elon.edu>
2024-05-01 14:54:37 -0400

    Mason Commit

--- main.cpp
+++ main.cpp
@@ -55,6 +55,9 @@ int main() {
     list<string>::iterator it = textList.begin();
     advance(it, rand_index);
     word = *it;
+    int wordSize = word.size();
+    string playingWord;
+    list <char> letterGuesses;
 
     // Add each character of the word into a list
     hangman.split(word, letters);
@@ -81,5 +84,72 @@ int main() {
 
     cout << "You ran out of guesses :(, better luck next time!" << endl;
 
+    for(int j = 0; j < wordSize; j++) {
+        playingWord += '_';
+    }
+
+    cout << 'Your Word: ' << playingWord << endl;
+
+    // Game loop
+    while (guesses > 0) {
+        char player_guess;
+
+        cout << "Guess a letter!" << endl;
+        cin >> player_guess;
+
+        guesses -= 1;
+
+        list <string> cheatingList;
+
+        bool guessRight = false;
+        for(char p : word) {
+            if(p == (player_guess)){
+                guessRight = true;
+            }
+        }
+
+        if(guessRight == true) {
+            //Makes list of all possible words to cheat with
+            for(int x = 0; x < textList.size(); x++) {
+
+                //Goes thru the list for if statement
+                list<string>::iterator it = textList.begin();
+                advance(it, 1);
+                string wordInList = *it;
+
+                //checks if the word is the same size and has the same of already guessed letters
+                if(wordInList.size() == word.size()) {
+                    bool stopper = false;
+                    for(int y = 0; y < word.size(); y++) {
+                        if(wordInList[y] != word[y] && word[y] != '_') {
+                            stopper = true;
+                        }
+                    }
+                    if(stopper == false) {
+                        cheatingList.push_front(word);
+                    }
+                }
+            }
+            if(cheatingList.empty() == false) {
+                cout << 'Hmm it seems you were wrong. Try again' << endl;
+            }
+            else {
+                for(int z = 0; z < playingWord.size();z++) {
+                    if(player_guess == word[z]) {
+                        playingWord = player_guess;
+                    }
+                }
+            }
+            if(playingWord == word) {
+                cout << 'You win....' << endl;
+                break;
+            }
+            letterGuesses.push_front(player_guess);
+        }
+        else {
+            cout << 'Wrong, try again' << endl;
+            letterGuesses.push_front(player_guess);
+        }
+    }
     return 0;
 }
