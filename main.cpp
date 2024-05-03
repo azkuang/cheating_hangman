a649dbfd49879ae265cd68dc8b4880821c883c6a
unknown <alexzkuang0314@gmail.com>
2024-05-03 11:44:00 -0400

    Somewhat working version of normal hangman, does not work well with duplicate letters.

--- main.cpp
+++ main.cpp
@@ -22,9 +22,9 @@ class HangMan {
             }
         }
 
-        // Creates an empty list
-        void create_empty(){
-            
+        // Method to turn a character into a string
+        string ToStr( char c ) {
+            return string( 1, c );
         }
 
         
@@ -64,33 +64,16 @@ int main() {
     // Add each character of the word into a list
     hangman.split(word, letters);
 
+    // Display the word for now (DELETE LATER)
+    cout<<word<<endl;
+
     cout << "The word has " << word.size() << " letters in it." << endl;
-
-    // Game loop
-    while (guesses > 0) {
-
-        char player_guess;
-        
-        cout << "Guess a letter!" << endl;
-        cin >> player_guess;
-
-        for (auto l:letters){
-            if (player_guess!=l){
-                guesses -= 1;
-            }
-            else {
-
-            }
-        }
-    }
-
-    cout << "You ran out of guesses :(, better luck next time!" << endl;
 
     for(int j = 0; j < wordSize; j++) {
         playingWord += '_';
     }
 
-    cout << 'Your Word: ' << playingWord << endl;
+    cout << "Your Word: " << playingWord << endl;
 
     // Game loop
     while (guesses > 0) {
@@ -99,59 +82,78 @@ int main() {
         cout << "Guess a letter!" << endl;
         cin >> player_guess;
 
-        guesses -= 1;
-
         list <string> cheatingList;
 
         bool guessRight = false;
+        int index_correct_word = 0;
         for(char p : word) {
             if(p == (player_guess)){
                 guessRight = true;
-            }
-        }
-
-        if(guessRight == true) {
-            //Makes list of all possible words to cheat with
-            for(int x = 0; x < textList.size(); x++) {
-
-                //Goes thru the list for if statement
-                list<string>::iterator it = textList.begin();
-                advance(it, 1);
-                string wordInList = *it;
-
-                //checks if the word is the same size and has the same of already guessed letters
-                if(wordInList.size() == word.size()) {
-                    bool stopper = false;
-                    for(int y = 0; y < word.size(); y++) {
-                        if(wordInList[y] != word[y] && word[y] != '_') {
-                            stopper = true;
-                        }
-                    }
-                    if(stopper == false) {
-                        cheatingList.push_front(word);
-                    }
-                }
-            }
-            if(cheatingList.empty() == false) {
-                cout << 'Hmm it seems you were wrong. Try again' << endl;
-            }
-            else {
-                for(int z = 0; z < playingWord.size();z++) {
-                    if(player_guess == word[z]) {
-                        playingWord = player_guess;
-                    }
-                }
-            }
-            if(playingWord == word) {
-                cout << 'You win....' << endl;
                 break;
             }
-            letterGuesses.push_front(player_guess);
+            index_correct_word++;
         }
+
+        if (guessRight==true){
+            list<char>::iterator it = letters.begin();
+            advance(it, index_correct_word);
+            char correct_letter = *it;
+            playingWord.replace(index_correct_word, 1, hangman.ToStr(correct_letter));
+        } 
         else {
-            cout << 'Wrong, try again' << endl;
-            letterGuesses.push_front(player_guess);
+            guesses--;
         }
+
+        cout << playingWord << endl;
+
+        // if(guessRight == true) {
+        //     //Makes list of all possible words to cheat with
+        //     for(int x = 0; x < textList.size(); x++) {
+
+        //         //Goes through the list for if statement
+        //         list<string>::iterator it = textList.begin();
+        //         advance(it, 1);
+        //         string wordInList = *it;
+
+        //         //checks if the word is the same size and has the same of already guessed letters
+        //         if(wordInList.size() == word.size()) {
+        //             bool stopper = false;
+        //             for(int y = 0; y < word.size(); y++) {
+        //                 if(wordInList[y] != word[y] && word[y] != '_') {
+        //                     stopper = true;
+        //                 }
+        //             }
+        //             if(stopper == false) {
+        //                 cheatingList.push_front(word);
+        //             }
+        //         }
+        //     }
+        //     if(cheatingList.empty() == false) {
+        //         cout << "Hmm it seems you were wrong. Try again" << endl;
+        //     }
+
+        //     else {
+        //         for(int z = 0; z < playingWord.size();z++) {
+        //             if(player_guess == word[z]) {
+        //                 playingWord = player_guess;
+        //             }
+        //         }
+        //     }
+
+        //     if(playingWord == word) {
+        //         cout << "You win...." << endl;
+        //         break;
+        //     }
+        //     letterGuesses.push_front(player_guess);
+        // }
+        // else {
+        //     cout << "Wrong, try again" << endl;
+        //     letterGuesses.push_front(player_guess);
+        // }
     }
+
+    cout << "You ran out of guesses :(, better luck next time!" << endl;
+
+
     return 0;
 }
